6f265aaa6a99d1f6e02d30c03cc2678d6ec2aa96
Eric <102000288+GrandEchoWhiskey@users.noreply.github.com>
2023-07-18 13:04:14 +0200

    Update models.py

--- proj-2-commerce/auctions/models.py
+++ proj-2-commerce/auctions/models.py
@@ -1,6 +1,32 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
 class User(AbstractUser):
     pass
+
+
+class Listing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=256)
+    image = models.CharField(max_length=1024, blank=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    category = models.CharField(max_length=64, blank=True)
+    active = models.BooleanField(default=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
+
+
+class Bid(models.Model):
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bids")
+
+
+class Comment(models.Model):
+    text = models.CharField(max_length=256)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="comments")
+
+
+class Watchlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="watchlists")
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="watchlists")
